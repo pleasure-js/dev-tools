63d9dd15b8bc0cc035abf104d5ba6c5613af616f
Martin Rafael Gonzalez <tin@devtin.io>
2019-08-03 15:48:20 -0400

    fix client initialization

--- test/admin-login.js
+++ test/admin-login.js
@@ -1,9 +1,9 @@
 const { PleasureApiClient } = require('pleasure-api-client')
 
-const pleasureClient = PleasureApiClient.instance()
-
 module.exports = {
   adminLogin () {
+    const pleasureClient = PleasureApiClient.instance()
+
     return pleasureClient.login({
       email: 'tin@devtin.io',
       password: 'aVeryStrongPassword123:)'
